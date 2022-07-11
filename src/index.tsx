21f8cde60a28a98402b5d6ed5c45a9ef133935f7
DullNebula <dvndull97@gmail.com>
2022-07-11 16:54:22 -0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Home, Dashboard, Signin} from './components';
+import './styles.css'
+
+
+let person = {
+  name: 'Devin',
+  codeName:'Nebula'
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home title = {'Cars Inventory'}/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
